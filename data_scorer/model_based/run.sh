b11bdfbc4a9e5df90729f2faf391b735a5926e72
hanzhang-lang <1214008796@qq.com>
2025-09-14 05:54:09 +0000

    .

--- data_scorer/model_based/run.sh
+++ data_scorer/model_based/run.sh
@@ -3,16 +3,16 @@
 python main_para.py --config configs/DeitaCScorer.yaml
 
 # DeitaQScorer
-python main_para.py --config configs/DeitaQScorer.yaml
+# python main_para.py --config configs/DeitaQScorer.yaml
 
-# IFDScorer
-python main_para.py --config configs/IFDScorer.yaml
+# # IFDScorer
+# python main_para.py --config configs/IFDScorer.yaml
 
-# RewardModel
-python main_para.py --config configs/RewardModel.yaml
+# # RewardModel
+# python main_para.py --config configs/RewardModel.yaml
 
-# ThinkingProbScorer
-python main_para.py --config configs/ThinkingProbScorer.yaml
+# # ThinkingProbScorer
+# python main_para.py --config configs/ThinkingProbScorer.yaml
 
-# Multi Scorers Togather
-python main_para.py --config configs/MultiScorer.yaml
+# # Multi Scorers Togather
+# python main_para.py --config configs/MultiScorer.yaml
